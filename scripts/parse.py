232ece54c59f4f93a04e9117614b80d9ba429346
Andras Horvath <horvathandrasmate@gmail.com>
2024-01-23 22:30:51 +0100

    upd algo

--- scripts/parse.py
+++ scripts/parse.py
@@ -2,10 +2,53 @@ import asyncio
 from prisma import Prisma
 from bs4 import BeautifulSoup
 from datetime import datetime
+import os
 async def main() -> None:
     db = Prisma()
     await db.connect()
-    with open("../data/upcoming.html", "r") as f:
+    #os.system("rm db.sqlite")
+    #os.system("prisma db push")
+    import glob 
+    years = glob.glob("data/years/*")
+
+    comps = []
+    for y in years:
+        with open(y, "r") as f:
+            html = f.read()
+            soup = BeautifulSoup(html, "html.parser")
+            table = soup.find_all("table")
+            for t in table:
+                #month level
+                trs = t.find_all("tr")
+                for tr in trs:
+                    #competition level
+                    #print(tr.prettify())
+                    a_s = tr.find_all("a")
+                    strongs = tr.find_all("strong")
+                    comp_date = datetime.strptime( strongs[0].text.replace(" ",""), '%Y.%m.%d')
+                    comp_title = strongs[1].text
+                    comp_place = strongs[2].text.split(":")[1]
+                    comp_categories = []
+                    #print(len(a_s))
+                    for a in range(1,len(a_s)):
+                        #competition detail level
+                        comp_categories.append(a_s[a].text)
+                    #print(comp_date, comp_title, comp_place, comp_categories)
+                    comps.append([comp_date, comp_title, comp_place, comp_categories])
+                    post = await db.competition.create(
+                            {
+                                'deadline' : datetime.fromtimestamp(0),
+                                'organiser' : "",
+                                'place' : comp_place,
+                                'title' : comp_title,
+                                'date' : comp_date 
+                            }
+                    )
+                    for cats in comp_categories:
+                        cat = await db.category.create({'name' : cats,
+                                'competition_id' : post.id
+                        })
+    with open("data/upcoming.html", "r") as f:
         html = f.read()
         soup = BeautifulSoup(html, "html.parser")
         table = soup.find_all("table")
@@ -47,6 +90,8 @@ async def main() -> None:
    
 
     await db.disconnect()
+    os.system("rm ../ksis/prisma/db.sqlite")
+    os.system("cp db.sqlite ../ksis/prisma/db.sqlite")
 
 
 if __name__ == '__main__':
